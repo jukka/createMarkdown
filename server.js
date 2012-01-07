9807d91632037787c44740d157bdee9b706088b5
Jukka Zitting <jukka@apache.org>
2012-01-07 13:21:14 +0100

    Add directory listings to the default server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 
 var server = express.createServer();
+server.use('/', express.directory(__dirname));
 server.use('/', express.static(__dirname));
 server.listen(3000);
